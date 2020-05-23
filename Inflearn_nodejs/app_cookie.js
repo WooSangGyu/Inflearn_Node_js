0483440341bc69f223ec8f28ce937938e4a27bad
WooSangGyu <wsg0701@naver.com>
2020-05-24 01:23:35 +0900

    make signedCookies function

--- Inflearn_nodejs/app_cookie.js
+++ Inflearn_nodejs/app_cookie.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
-app.use(cookieParser());
+app.use(cookieParser('24@!$!@$12412412$A124')); //24@!$!@$12412412$A124 는 암호화의 키값
 
 var products = {
     1:{title:'The history of web 1'},
@@ -28,30 +28,30 @@ cart = {
 }
 */
 app.get('/cart/:id', function(req, res){
-    var id = req.params.id;
-    if(req.cookies.cart) {
-        var cart = req.cookies.cart;
+    var id = req.params.id;      //cart 의 아이디 값 저장
+    if(req.signedCookies.cart) {              // 만약 cart의 값이 있다면 그대로 가져와서 적용 없다면 새롭게 cart의 값을 만듦
+        var cart = req.signedCookies.cart;
     } else {
       var cart = {};
     }
     
-    if(!cart[id]) {
+    if(!cart[id]) {       //만약 cart[id]의 값이 없다면 만들고 0 으로 지정
         cart[id] = 0;
     }
-    cart[id] = parseInt(cart[id])+1;
+    cart[id] = parseInt(cart[id])+1;   //cart[id]값에 현재 갯수+1해서 값 증가시키기
 
-    res.cookie('cart', cart)
-    res.redirect('/cart');
+    res.cookie('cart', cart, {signed:true})     // cookie의 'cart' 항목에 cart의 값을 저장 , singed:true는 암호화 된 쿠키를 풀라는 뜻
+    res.redirect('/cart'); // localhost3003/cart로 넘겨주기
 });
 
 app.get('/cart', function(req, res){
-    var cart = req.cookies.cart;
-    if (!cart) {
-        res.send('Empty');
+    var cart = req.signedCookies.cart;
+    if (!cart) {                    // cart 안에 값이 없다면 비어있다고 알려주기
+        res.send('Empty'); 
     } else {
         var output = '';
         for(var id in cart) {
-            output += `<li>${products[id].title} (${cart[id]})</li>`
+            output += `<li>${products[id].title} (${cart[id]})</li>` //products[id].title로 상품 이름 가져오기, cart[id]로 id값의 개수 가져오기
         }
     }
     res.send(`
@@ -63,15 +63,15 @@ app.get('/cart', function(req, res){
 
 
 app.get('/count', function(req, res) {
-    if(req.cookies.count) {
-        var count = parseInt(req.cookies.count);
+    if(req.signedCookies.count) {
+        var count = parseInt(req.signedCookies.count);
     } else {
         var count = 0;
     }
 
     count = count+1;
 
-    res.cookie('count',count);
+    res.cookie('count',count , {signed:true});
     res.send('count : ' + count);
 });
 
